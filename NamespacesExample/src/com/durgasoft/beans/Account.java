86608fd1a22d2d9dadbf313aa42d3c71d6dec067
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2023-12-16 20:20:30 +0530

    committed

--- NamespacesExample/src/com/durgasoft/beans/Account.java
+++ NamespacesExample/src/com/durgasoft/beans/Account.java
@@ -1,5 +1,39 @@
 package com.durgasoft.beans;
 
 public class Account {
-
+	private String accNo;
+	private String accName;
+	private String accType;
+	private float accBal;
+	
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+	public String getAccType() {
+		return accType;
+	}
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}
+	public float getAccBal() {
+		return accBal;
+	}
+	public void setAccBal(float accBal) {
+		this.accBal = accBal;
+	}
+	public void getAccDetails() {
+		System.out.println("------Account Details------");
+		System.out.println("Account Number:"+accNo);
+		System.out.println("Account Balance:"+accBal);
+		System.out.println("Account Type:"+accType);
+	}
 }
--- NamespacesExample/src/com/durgasoft/beans/Employee.java
+++ NamespacesExample/src/com/durgasoft/beans/Employee.java
@@ -1,5 +1,53 @@
 package com.durgasoft.beans;
 
 public class Employee {
-
+	private String empId;
+	private String empName;
+	private String empSal;
+	private Account empAcc;
+	private String empAddr;
+	
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(String empSal) {
+		this.empSal = empSal;
+	}
+	public Account getEmpAcc() {
+		return empAcc;
+	}
+	public void setEmpAcc(Account empAcc) {
+		this.empAcc = empAcc;
+	}
+	public String getEmpAddr() {
+		return empAddr;
+	}
+	public void setEmpAddr(String empAddr) {
+		this.empAddr = empAddr;
+	}
+	
+	public void getEmpDetails() {
+		System.out.println("***********Employee Details*****************");
+		System.out.println("Employee Id: "+empId);
+		System.out.println("Employee Name: "+empName);
+		System.out.println("Employee Salary: "+empSal);
+		System.out.println("Employee Address: "+empAddr);
+		System.out.println("------Employee Account Details------");
+		System.out.println("Employee Account Number:"+empAcc.getAccNo());
+		System.out.println("Employee Account Balance:"+empAcc.getAccBal());
+		System.out.println("Employee Account Type:"+empAcc.getAccType());
+		
+	}
 }
