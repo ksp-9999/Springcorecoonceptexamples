ece207638a123d584d5fc68d78010ee87baf715a
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2023-12-17 13:06:41 +0530

    committed

--- XmlAWExample/src/com/durgasoft/beans/Employee.java
+++ XmlAWExample/src/com/durgasoft/beans/Employee.java
@@ -7,37 +7,44 @@ public class Employee {
 	private Account account;
 	private Address address;
 	
-	public Account getAccount() {
-		return account;
+	public Employee(String eid,String eName,float eSal,Account account,Address address) {
+		this.eid=eid;
+		this.eName=eName;
+		this.eSal=eSal;
+		this.account=account;
+		this.address=address;
 	}
-	public void setAccount(Account account) {
-		this.account = account;
-	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-	public String getEid() {
-		return eid;
-	}
-	public void setEid(String eid) {
-		this.eid = eid;
-	}
-	public String geteName() {
-		return eName;
-	}
-	public void seteName(String eName) {
-		this.eName = eName;
-	}
-	public float geteSal() {
-		return eSal;
-	}
-	public void seteSal(float eSal) {
-		this.eSal = eSal;
-	}
-	
+//	public Account getAccount() {
+//		return account;
+//	}
+//	public void setAccount(Account account) {
+//		this.account = account;
+//	}
+//	public Address getAddress() {
+//		return address;
+//	}
+//	public void setAddress(Address address) {
+//		this.address = address;
+//	}
+//	public String getEid() {
+//		return eid;
+//	}
+//	public void setEid(String eid) {
+//		this.eid = eid;
+//	}
+//	public String geteName() {
+//		return eName;
+//	}
+//	public void seteName(String eName) {
+//		this.eName = eName;
+//	}
+//	public float geteSal() {
+//		return eSal;
+//	}
+//	public void seteSal(float eSal) {
+//		this.eSal = eSal;
+//	}
+//	
 	
 	public void getEmpDetails() {
 		System.out.println("*********Employee Details**************");
