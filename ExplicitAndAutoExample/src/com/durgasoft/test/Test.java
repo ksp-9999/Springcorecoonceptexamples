7f998092c47d9c87ac2c98faa3d5e3778e7b8f35
saiprasadkilaru <kilarusaiprasad19@gmail.com>
2023-12-20 09:00:35 +0530

    committed

--- ExplicitAndAutoExample/src/com/durgasoft/test/Test.java
+++ ExplicitAndAutoExample/src/com/durgasoft/test/Test.java
@@ -1,9 +1,15 @@
 package com.durgasoft.test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.durgasoft.beans.Student;
+
+import org.springframework.context.ApplicationContext;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationContext context= new ClassPathXmlApplicationContext("com/durgasoft/resources/applicationContext.xml");
+		Student std= (Student) context.getBean("studentBean");
+		std.getStudentDetails();
 
 	}
 
